6c8d3febbaa1206e6a510498eb7d9bb34d3aa9c7
Rishav Kumar <rishavkumar18902@gmail.com>
2025-03-22 00:28:27 +0530

    global css modified

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,6 +1,6 @@
 import FeaturedPage from "@/components/FeaturedPage";
 import HeroSection from "@/components/HeroSection";
-
+import Login from "@/components/Login";
 
 
 export default function Home() {
@@ -8,7 +8,7 @@ export default function Home() {
     <main className="min-h-screen bg-black antialiased bg-grid-white/[0.02]">
       <HeroSection />
       <FeaturedPage />
-
+      <Login />
     </main>
   );
 }
